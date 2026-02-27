be2620c576ce8e82e0faf4a111d1951d4c6d06dd
isho-0 <isho010607@gmail.com>
2026-02-28 02:21:59 +0900

    feat: [PCCE 기출문제] 1번 / 문자 출력

---
+++ Programmers/main.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void solution_340207();
+
+int main() {
+    return 0;
+}
---
+++ Programmers/solution_340207.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void solution_340207() {
+	string message = "Let's go!";
+
+	cout << "3\n2\n1" << endl;
+	cout << message << endl;
+}
